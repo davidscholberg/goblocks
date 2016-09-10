f95c18c1ee0d39a24dcf5596df19299f89ed84ea
David Scholberg <recombinant.vector@gmail.com>
2016-09-10 16:49:40 -0400

    replaced fmt.Fprintf calls with Logger.Fatalln

--- goblocks.go
+++ goblocks.go
@@ -4,29 +4,29 @@ import (
 	"fmt"
 	"github.com/davidscholberg/go-i3barjson"
 	"github.com/davidscholberg/goblocks/lib/modules"
+	"log"
 	"os"
 	"reflect"
 )
 
 func main() {
+	errLogger := log.New(os.Stderr, "error: ", 0)
+
 	gb, err := modules.NewGoblocks()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s", err)
-		return
+		errLogger.Fatalln(err)
 	}
 
 	h := i3barjson.Header{Version: 1}
 	err = i3barjson.Init(os.Stdout, nil, h, gb.Cfg.Global.Debug)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s", err)
-		return
+		errLogger.Fatalln(err)
 	}
 
 	// send the first statusLine
 	err = i3barjson.Update(gb.StatusLine)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s", err)
-		return
+		errLogger.Fatalln(err)
 	}
 
 	for {
@@ -34,28 +34,24 @@ func main() {
 		i, _, _ := reflect.Select(gb.SelectCases.Cases)
 		selectReturn := gb.SelectCases.Actions[i](gb.SelectCases.Blocks[i])
 		if selectReturn.Exit {
+			fmt.Println("")
 			break
 		}
 		if selectReturn.Refresh {
 			err = i3barjson.Update(gb.StatusLine)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s", err)
-				break
+				errLogger.Fatalln(err)
 			}
 		} else if selectReturn.Reload {
 			gb.Reset()
 			gb, err = modules.NewGoblocks()
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s", err)
-				break
+				errLogger.Fatalln(err)
 			}
 			err = i3barjson.Update(gb.StatusLine)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s", err)
-				break
+				errLogger.Fatalln(err)
 			}
 		}
 	}
-
-	fmt.Println("")
 }
