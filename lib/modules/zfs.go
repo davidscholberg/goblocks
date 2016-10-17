b2c8339cbf3f446d92e9e09c9f2bea91d95e3bfe
Zachary Dykstra <dykstra.zachary@gmail.com>
2016-10-17 17:19:54 -0500

    Initial ZFS commit

---
+++ lib/modules/zfs.go
@@ -0,0 +1,59 @@
+package modules
+
+import (
+  "github.com/davidscholberg/go-i3barjson"
+  "fmt"
+  "os/exec"
+  "bufio"
+  "strings"
+)
+
+type Zfs struct {
+  BlockConfigBase `yaml:",inline"`
+  PoolName        string `yaml:"zpool_name"`
+}
+
+func (c Zfs) UpdateBlock(b *i3barjson.Block) {
+  b.Color = c.Color
+  fullTextFmt := fmt.Sprintf("%s%s - %%s", c.Label, c.PoolName)
+
+  zpoolCmd := exec.Command("sudo","/sbin/zpool","status", c.PoolName)
+  out,err := zpoolCmd.StdoutPipe()
+
+  if err != nil {
+    b.Urgent = true
+    b.FullText = fmt.Sprintf(fullTextFmt,err.Error())
+    return
+  }
+
+  if err := zpoolCmd.Start(); err != nil {
+    b.Urgent = true
+    b.FullText = fmt.Sprintf(fullTextFmt,err.Error())
+    return
+  }
+
+  defer zpoolCmd.Wait()
+
+  buff := bufio.NewScanner(out)
+  for buff.Scan() {
+    line := strings.TrimSpace(buff.Text())
+    if strings.HasPrefix(line,"state") {
+      split := strings.Split(line,":")
+      status := strings.TrimSpace(split[1])
+
+      if status == "ONLINE" {
+        b.Urgent = false
+      } else {
+        b.Urgent = true
+      }
+      b.FullText = fmt.Sprintf(fullTextFmt,status)
+      return 
+    }
+  }
+
+  b.Urgent = true
+  b.FullText = fmt.Sprintf(fullTextFmt,"NOT FOUND")
+  return
+}
+
+
