4758d8457613eb1e568ab0881fe131ba61c355db
David Scholberg <recombinant.vector@gmail.com>
2016-10-30 12:29:54 -0400

    changed key indicator to single key per block, added color support

--- lib/modules/keyindicator.go
+++ lib/modules/keyindicator.go
@@ -10,7 +10,10 @@ import (
 // KeyIndicator represents the configuration for the key indicator block.
 type KeyIndicator struct {
 	BlockConfigBase `yaml:",inline"`
-	Keys            []string `yaml:"keys"`
+	Key             string `yaml:"key"`
+	KeyText         string `yaml:"key-text"`
+	OnColor         string `yaml:"on-color"`
+	OffColor        string `yaml:"off-color"`
 }
 
 // UpdateBlock updates the key indicator block's status.
@@ -26,64 +29,69 @@ func (c KeyIndicator) UpdateBlock(b *i3barjson.Block) {
 		return
 	}
 
-	keyStatuses := make(map[string]bool)
+	keyFound := false
+	keyStatus := false
 	xsetLines := strings.Split(string(out), "\n")
 	for _, xsetLine := range xsetLines {
-		for _, keyStr := range c.Keys {
-			if _, ok := keyStatuses[keyStr]; ok {
-				continue
-			}
-			keyIndex := strings.Index(xsetLine, keyStr)
-			if keyIndex == -1 {
-				continue
-			}
-			xsetLineSubstr := xsetLine[keyIndex+len(keyStr):]
-			keyStatus := strings.Index(xsetLineSubstr, "o")
-			if keyStatus == -1 {
-				b.Urgent = true
-				b.FullText = fmt.Sprintf(
-					fullTextFmt,
-					fmt.Sprintf(
-						"couldn't find status for key '%s'",
-						keyStr,
-					),
-				)
-				return
-			}
-			switch xsetLineSubstr[keyStatus+1 : keyStatus+2] {
-			case "n":
-				keyStatuses[keyStr] = true
-			case "f":
-				keyStatuses[keyStr] = false
-			default:
-				b.Urgent = true
-				b.FullText = fmt.Sprintf(
-					fullTextFmt,
-					fmt.Sprintf(
-						"unknown status for key '%s'",
-						keyStr,
-					),
-				)
-				return
-			}
+		keyIndex := strings.Index(xsetLine, c.Key)
+		if keyIndex == -1 {
+			continue
 		}
-		if len(keyStatuses) == len(c.Keys) {
+		xsetLineSubstr := xsetLine[keyIndex+len(c.Key):]
+		keyStatusIndex := strings.Index(xsetLineSubstr, "o")
+		if keyStatusIndex == -1 {
+			b.Urgent = true
+			b.FullText = fmt.Sprintf(
+				fullTextFmt,
+				fmt.Sprintf(
+					"couldn't find status for key '%s'",
+					c.Key,
+				),
+			)
+			return
+		}
+		switch xsetLineSubstr[keyStatusIndex+1 : keyStatusIndex+2] {
+		case "n":
+			keyFound = true
+			keyStatus = true
+		case "f":
+			keyFound = true
+			keyStatus = false
+		default:
+			b.Urgent = true
+			b.FullText = fmt.Sprintf(
+				fullTextFmt,
+				fmt.Sprintf(
+					"unknown status for key '%s'",
+					c.Key,
+				),
+			)
+			return
+		}
+		if keyFound {
 			break
 		}
 	}
-	var keysOn []string
-	for keyStr, keyStatus := range keyStatuses {
-		if keyStatus {
-			keysOn = append(keysOn, keyStr)
-		}
-	}
-	var keysOnStr string
-	if len(keysOn) > 0 {
+
+	if !keyFound {
 		b.Urgent = true
-		keysOnStr = strings.Join(keysOn, ", ")
-	} else {
-		b.Urgent = false
-		keysOnStr = "none"
+		b.FullText = fmt.Sprintf(
+			fullTextFmt,
+			fmt.Sprintf(
+				"couldn't find key '%s'",
+				c.Key,
+			),
+		)
+		return
 	}
-	b.FullText = fmt.Sprintf(fullTextFmt, keysOnStr)
+
+	b.Urgent = false
+
+	if keyStatus {
+		b.Color = c.OnColor
+	} else {
+		b.Color = c.OffColor
+	}
+
+	b.FullText = fmt.Sprintf(fullTextFmt, c.KeyText)
 }
