c0da3d84701cfaedff8e5775f97a09311f86a694
Magnus Kaiser <info@magnuskaiser.com>
2017-01-08 02:06:56 +0100

    added more iface details

--- lib/modules/iface.go
+++ lib/modules/iface.go
@@ -3,38 +3,62 @@ package modules
 import (
 	"fmt"
 	"github.com/davidscholberg/go-i3barjson"
-	"os"
+	"net"
+	"strings"
 )
 
 // Interface represents the configuration for the network interface block.
 type Interface struct {
 	BlockConfigBase `yaml:",inline"`
 	IfaceName       string `yaml:"interface_name"`
+	IPv4            string
+	IPv4CIDR        string
+	IPv6            string
+	IPv6CIDR        string
 }
 
 // UpdateBlock updates the network interface block.
 func (c Interface) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
-	var statusStr string
-	sysFilePath := fmt.Sprintf("/sys/class/net/%s/operstate", c.IfaceName)
-	r, err := os.Open(sysFilePath)
+
+	iface, err := net.InterfaceByName(c.IfaceName)
 	if err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
 	}
-	defer r.Close()
-	_, err = fmt.Fscanf(r, "%s", &statusStr)
-	if err != nil {
-		b.Urgent = true
-		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
-		return
-	}
-	if statusStr == "up" {
+
+	if iface.Flags == net.FlagUp {
 		b.Urgent = false
 	} else {
 		b.Urgent = true
 	}
-	b.FullText = fmt.Sprintf(fullTextFmt, statusStr)
+
+	addrs, err := iface.Addrs()
+	if err != nil {
+		b.Urgent = true
+		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
+		return
+	}
+
+	for _, addr := range addrs {
+		ip, _, err := net.ParseCIDR(addr.String())
+		if err != nil {
+			b.Urgent = true
+			b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
+			return
+		}
+
+		// Checking for address family
+		if ip.To4() != nil {
+			c.Label = strings.Replace(c.Label, "\u003cipv4\u003e", ip.String(), -1)
+			c.Label = strings.Replace(c.Label, "\u003ccidr4\u003e", addr.String(), -1)
+		} else {
+			c.Label = strings.Replace(c.Label, "\u003cipv6\u003e", ip.String(), -1)
+			c.Label = strings.Replace(c.Label, "\u003ccidr6\u003e", addr.String(), -1)
+		}
+	}
+
+	b.FullText = fmt.Sprintf(fullTextFmt, c.Label)
 }
