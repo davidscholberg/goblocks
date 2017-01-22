479f32437a7056529af2265a0f430f65510175dc
Magnus Kaiser <info@magnuskaiser.com>
2017-01-22 20:41:31 +0100

    added status & local6 placeholder; some improvements

--- lib/modules/iface.go
+++ lib/modules/iface.go
@@ -15,10 +15,15 @@ type Interface struct {
 	IPv4CIDR        string
 	IPv6            string
 	IPv6CIDR        string
+	IPv6Local       string
 }
 
 // UpdateBlock updates the network interface block.
 func (c Interface) UpdateBlock(b *i3barjson.Block) {
+	var (
+		status string
+	)
+
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
 
@@ -31,8 +36,10 @@ func (c Interface) UpdateBlock(b *i3barjson.Block) {
 
 	if iface.Flags == net.FlagUp {
 		b.Urgent = false
+		status = "up"
 	} else {
 		b.Urgent = true
+		status = "down"
 	}
 
 	addrs, err := iface.Addrs()
@@ -52,13 +59,30 @@ func (c Interface) UpdateBlock(b *i3barjson.Block) {
 
 		// Checking for address family
 		if ip.To4() != nil {
-			c.Label = strings.Replace(c.Label, "\u003cipv4\u003e", ip.String(), -1)
-			c.Label = strings.Replace(c.Label, "\u003ccidr4\u003e", addr.String(), -1)
+			fullTextFmt = strings.Replace(fullTextFmt, "\u003cipv4\u003e", ip.String(), -1)
+			fullTextFmt = strings.Replace(fullTextFmt, "\u003ccidr4\u003e", addr.String(), -1)
 		} else {
-			c.Label = strings.Replace(c.Label, "\u003cipv6\u003e", ip.String(), -1)
-			c.Label = strings.Replace(c.Label, "\u003ccidr6\u003e", addr.String(), -1)
+
+			if ip.String()[0:4] == "fe80" {
+				//setting ipv6 link local
+				fullTextFmt = strings.Replace(fullTextFmt, "\u003clocal6\u003e", ip.String(), -1)
+			} else {
+				fullTextFmt = strings.Replace(fullTextFmt, "\u003cipv6\u003e", ip.String()[0:3], -1)
+				fullTextFmt = strings.Replace(fullTextFmt, "\u003ccidr6\u003e", addr.String(), -1)
+			}
 		}
 	}
 
-	b.FullText = fmt.Sprintf(fullTextFmt, c.Label)
+	// setting up/down flag
+	fullTextFmt = strings.Replace(fullTextFmt, "\u003cstatus\u003e", status, -1)
+
+	// clearing unset fields i.e. because of ipv6 single-stack
+	fullTextFmt = strings.Replace(fullTextFmt, "\u003cipv4\u003e", "", -1)
+	fullTextFmt = strings.Replace(fullTextFmt, "\u003ccidr4\u003e", "", -1)
+	fullTextFmt = strings.Replace(fullTextFmt, "\u003cipv6\u003e", "", -1)
+	fullTextFmt = strings.Replace(fullTextFmt, "\u003ccidr6\u003e", "", -1)
+	fullTextFmt = strings.Replace(fullTextFmt, "\u003clocal6\u003e", "", -1)
+
+	// removing the last %s placeholder from final string
+	b.FullText = fmt.Sprintf(fullTextFmt, "")
 }
