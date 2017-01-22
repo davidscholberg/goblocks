a02fcfce91cfb0e7b5830dcd76b6ea9ba65f1378
David Scholberg <recombinant.vector@gmail.com>
2017-01-22 11:57:22 -0500

    added custom duration formatting for uptime module

--- lib/modules/uptime.go
+++ lib/modules/uptime.go
@@ -2,15 +2,16 @@ package modules
 
 import (
 	"fmt"
+	"github.com/davidscholberg/go-durationfmt"
+	"github.com/davidscholberg/go-i3barjson"
 	"os"
 	"time"
-
-	"github.com/davidscholberg/go-i3barjson"
 )
 
 // Uptime represents the configuration for the time display block.
 type Uptime struct {
 	BlockConfigBase `yaml:",inline"`
+	DurationFormat  string `yaml:"duration_format"`
 }
 
 // UpdateBlock updates the time display block.
@@ -31,9 +32,20 @@ func (c Uptime) UpdateBlock(b *i3barjson.Block) {
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
 	}
+	dur := time.Duration(load) * time.Second
+	durFmt := c.DurationFormat
+	if durFmt == "" {
+		durFmt = "%hh%mm%ss"
+	}
+	durStr, err := durationfmt.Format(dur, durFmt)
+	if err != nil {
+		b.Urgent = true
+		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
+		return
+	}
 	b.FullText = fmt.Sprintf(
 		"%s%s",
 		c.Label,
-		time.Duration(load)*time.Second,
+		durStr,
 	)
 }
