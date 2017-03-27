f1e83e180e642387b8b2a9255522217b7fbc1e02
Zachary Dykstra <dykstra.zachary@gmail.com>
2017-03-27 16:58:28 -0500

    Allow the amixer mixer name to be changed
    This allows amixer to report the PulseAudio default mixer volume. 
    Set to previous behavior

--- lib/modules/volume.go
+++ lib/modules/volume.go
@@ -10,6 +10,7 @@ import (
 // Volume represents the configuration for the volume display block.
 type Volume struct {
 	BlockConfigBase `yaml:",inline"`
+	MixerType       string `yaml:"mixer_type"`
 }
 
 // UpdateBlock updates the volume display block.
@@ -18,7 +19,7 @@ func (c Volume) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
 	amixerCmd := "amixer"
-	amixerArgs := []string{"-D", "default", "get", "Master"}
+	amixerArgs := []string{"-D", c.MixerType, "get", "Master"}
 	out, err := exec.Command(amixerCmd, amixerArgs...).Output()
 	if err != nil {
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
