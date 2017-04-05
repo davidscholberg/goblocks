0515b343a384afc29505e15e65706709e6c9bd63
Mitch Roote <mitch@r00t.ca>
2017-04-05 19:38:05 -0400

    Parse command arguments, add example to config file

--- lib/modules/command.go
+++ lib/modules/command.go
@@ -16,8 +16,11 @@ type Command struct {
 func (c Command) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
-	out, err := exec.Command(c.Cmd).Output()
-	if err != nil {
+
+	cmdParse := strings.Fields(c.Cmd)
+	cmd, args := cmdParse[0], cmdParse[1:]
+
+	if out, err := exec.Command(cmd, args...).Output(); err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
