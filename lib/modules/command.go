6d97b97cc36fcb78e7bebd9fba205934d8a8d01b
Mitch Roote <mitch@r00t.ca>
2017-04-15 22:33:59 -0400

    Use TrimSpace to remove whitespace from cmd output

--- lib/modules/command.go
+++ lib/modules/command.go
@@ -14,17 +14,19 @@ type Command struct {
 }
 
 func (c Command) UpdateBlock(b *i3barjson.Block) {
+	var cmdOutput []byte
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
 
 	cmdParse := strings.Fields(c.Cmd)
 	cmd, args := cmdParse[0], cmdParse[1:]
 
-	if out, err := exec.Command(cmd, args...).Output(); err != nil {
+	cmdOutput, err := exec.Command(cmd, args...).Output()
+	if err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
 	}
 
-	b.FullText = fmt.Sprintf(fullTextFmt, strings.Replace(string(out), "\n", "", -1))
+	b.FullText = fmt.Sprintf(fullTextFmt, strings.TrimSpace(string(cmdOutput)))
 }
